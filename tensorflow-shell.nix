573d91d9bdb71d0e28de6877f572abfda9574b15
Roman Salmin <Roman.Salmin@gmail.com>
2019-11-13 20:20:03 +0300

    minor change

--- tensorflow-shell.nix
+++ tensorflow-shell.nix
@@ -1,6 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
 let
-     python = pkgs.python37;
      keras-applications = pkgs.callPackage ./tf/keras-applications.nix {
         buildPythonPackage = pkgs.python37Packages.buildPythonPackage;
         inherit (pkgs.python37Packages) fetchPypi numpy h5py;
@@ -12,7 +11,7 @@ let
      tensorflow = pkgs.callPackage ./tf/tensorflow.nix {
         python = pkgs.python37;
         buildPythonPackage = pkgs.python37Packages.buildPythonPackage;
-        inherit (python.passthru) isPy3k pythonAtLeast pythonOlder;
+        inherit (pkgs.python37.passthru) isPy3k pythonAtLeast pythonOlder;
         inherit (pkgs.linuxPackages) nvidia_x11;
         inherit (pkgs.python37Packages) numpy tensorflow-tensorboard backports_weakref mock enum34 absl-py future setuptools wheel keras-preprocessing google-pasta functools32 opt-einsum termcolor grpcio six wrapt cython astor gast;
         keras-applications = keras-applications;
